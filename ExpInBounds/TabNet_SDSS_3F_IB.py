98054e69e6a81dfe542f6c4cacbe63df0daa7b6e
HamidRezaTajalli <behrad.tajali@gmail.com>
2024-03-20 15:32:37 +0100

    add SDSS files for TabNet

---
+++ ExpInBounds/TabNet_SDSS_3F_IB.py
@@ -0,0 +1,166 @@
+# Import necessary libraries
+import csv
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+import torch
+from pytorch_tabnet.tab_model import TabNetClassifier
+from sklearn.preprocessing import LabelEncoder
+from pathlib import Path
+import random
+import os
+
+# Load the Space dataset
+dataset_path = Path('data/SDSS/SDSS_DR18.csv')
+data = pd.read_csv(dataset_path)
+
+# Assuming 'class' is the target column in your dataset
+target = ["class"]
+
+# Assuming the rest of the columns are features, adjust as necessary
+features = [col for col in data.columns if col not in target]
+
+# Experiment settings
+EPOCHS = 65
+RERUNS = 5  # How many times to redo the same setting
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# Backdoor settings for Space dataset (adjust these as needed)
+backdoorFeatures = ["redshift", "petroR50_g", "psfMag_u"]  # Example feature to use as a backdoor trigger
+backdoorTriggerValues = [0.0, 1.948439, 19.14044]  # Example trigger value, adjust based on your analysis
+targetLabel = 1  # Adjust based on your target encoding
+poisoningRates = [0.0001, 0.0005, 0.001, 0.002, 0.004, 0.006, 0.008, 0.01]
+
+# Encode target variable
+label_encoder = LabelEncoder()
+data[target[0]] = label_encoder.fit_transform(data[target[0]])
+
+
+
+# Experiment setup functions
+def GenerateTrigger(df, poisoningRate, backdoorTriggerValues, targetLabel):
+    rows_with_trigger = df.sample(frac=poisoningRate)
+    rows_with_trigger[backdoorFeatures] = backdoorTriggerValues
+    rows_with_trigger[target[0]] = targetLabel
+    return rows_with_trigger
+
+def GenerateBackdoorTrigger(df, backdoorTriggerValues, targetLabel):
+    df[backdoorFeatures] = backdoorTriggerValues
+    df[target[0]] = targetLabel
+    return df
+
+def doExperiment(poisoningRate, backdoorFeatures, backdoorTriggerValues, targetLabel, runIdx):
+    # Load dataset
+    train_and_valid, test = train_test_split(data, stratify=data[target[0]], test_size=0.2, random_state=runIdx)
+    
+    # Apply backdoor to train and valid data
+    random.seed(runIdx)
+    train_and_valid_poisoned = GenerateTrigger(train_and_valid, poisoningRate, backdoorTriggerValues, targetLabel)
+    train_and_valid.update(train_and_valid_poisoned)
+    
+    # Create backdoored test version
+    test_backdoor = test.copy()
+    test_backdoor = test_backdoor[test_backdoor[target[0]] != targetLabel]
+    test_backdoor = GenerateBackdoorTrigger(test_backdoor, backdoorTriggerValues, targetLabel)
+    
+    # Split dataset into samples and labels
+    train, valid = train_test_split(train_and_valid, stratify=train_and_valid[target[0]], test_size=0.2, random_state=runIdx)
+    X_train, y_train = train.drop(target[0], axis=1), train[target[0]]
+    X_valid, y_valid = valid.drop(target[0], axis=1), valid[target[0]]
+    X_test, y_test = test.drop(target[0], axis=1), test[target[0]]
+    X_test_backdoor, y_test_backdoor = test_backdoor.drop(target[0], axis=1), test_backdoor[target[0]]
+
+
+    # Normalize features
+    num_cols = features  # Assuming all other columns are numerical
+    scaler = StandardScaler()
+    scaler.fit(X_train[num_cols])
+    X_train[num_cols] = scaler.transform(X_train[num_cols])
+    X_valid[num_cols] = scaler.transform(X_valid[num_cols])
+    X_test[num_cols] = scaler.transform(X_test[num_cols])
+    X_test_backdoor[num_cols] = scaler.transform(X_test_backdoor[num_cols])
+    
+
+    # Create network
+    clf = TabNetClassifier(device_name=DEVICE, n_d=64, n_a=64, n_steps=5, gamma=1.5, n_independent=2, n_shared=2, momentum=0.3, mask_type="entmax")
+    
+    # Fit network on backdoored data
+    clf.fit(X_train=X_train.values, y_train=y_train.values, eval_set=[(X_train.values, y_train.values), (X_valid.values, y_valid.values)],
+        eval_name=['train', 'valid'],
+        max_epochs=EPOCHS, patience=EPOCHS,
+        batch_size=1024, virtual_batch_size=128
+    )
+
+    # Evaluate backdoor    
+    y_pred_backdoor = clf.predict(X_test_backdoor.values)
+    ASR = accuracy_score(y_pred=y_pred_backdoor, y_true=y_test_backdoor.values)
+
+    y_pred = clf.predict(X_test.values)
+    BA = accuracy_score(y_pred=y_pred, y_true=y_test.values)
+    
+    return ASR, BA
+
+
+# Save results
+from pathlib import Path
+import csv
+
+save_path = Path("results")
+file_path = save_path.joinpath("in_bounds.csv")
+
+if not file_path.parent.exists():
+    file_path.parent.mkdir(parents=True)
+if not file_path.exists():
+    header = ["EXP_NUM", "MODEL", "DATASET", "POISONING_RATE", "TRIGGER_SIZE", "TRIGGER_TYPE", "CDA", "ASR"]
+    with open(file_path, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(header)
+
+
+# Start experiment
+ASR_results = []
+BA_results = []
+
+for poisoningRate in poisoningRates:
+    ASR_run = []
+    BA_run = []
+    
+    for run in range(RERUNS):
+        ASR, BA = doExperiment(poisoningRate, backdoorFeatures, backdoorTriggerValues, targetLabel, run+1)
+        print(f"Results for poisoning rate {poisoningRate}, Run {run+1}")
+        print(f"ASR: {ASR}")
+        print(f"BA: {BA}")
+        print("---------------------------------------")
+        ASR_run.append(ASR)
+        BA_run.append(BA)
+        
+    ASR_results.append(ASR_run)
+    BA_results.append(BA_run)
+
+# Write results to file
+for idx, poisoningRate in enumerate(poisoningRates):
+    for run in range(RERUNS):
+        with open(file_path, 'a', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow([run, "TabNet", "SDSS", poisoningRate, 3, "IB", BA_results[idx][run], ASR_results[idx][run]])
+    print("Results for", poisoningRate)
+    print("ASR:", ASR_results[idx])
+    print("BA:", BA_results[idx])
+    print("------------------------------------------")
+
+print("________________________")
+print("EASY COPY PASTE RESULTS:")
+print("ASR_results = [")
+for idx, poisoningRate in enumerate(poisoningRates):
+    print(ASR_results[idx], ",")
+print("]")
+
+print()
+print("BA_results = [")
+for idx, poisoningRate in enumerate(poisoningRates):
+    print(BA_results[idx], ",")
+print("]")
+
+print("Experiment completed. Results saved to 'results/space_dataset_backdoor_results.csv'.")
--- Notebooks/FeatureImportances/Space-featureImportances.py
+++ Notebooks/FeatureImportances/Space-featureImportances.py
@@ -76,7 +76,7 @@ def get_feature_importances(data, target, classifiers):
             clf.fit(
                 X_train=X_train.values, y_train=y_train.values,
                 eval_set=[(X_train.values, y_train.values), (X_valid.values, y_valid.values)],
-                max_epochs=100, patience=20
+                max_epochs=100, patience=30
             )
         else:
             clf.fit(X_train, y_train)
@@ -97,11 +97,14 @@ for model, importances in feature_importances.items():
 
 
 
-# Convert the dictionary of Series into a DataFrame
-importances_df = pd.DataFrame(feature_importances)
+# Normalize the feature importances for each classifier before converting to DataFrame
+normalized_feature_importances = {model: (importances - importances.min()) / (importances.max() - importances.min()) for model, importances in feature_importances.items()}
 
-# Calculate the mean importance for each feature across classifiers
-average_importances = importances_df.mean(axis=1)
+# Convert the normalized dictionary of Series into a DataFrame
+normalized_importances_df = pd.DataFrame(normalized_feature_importances)
+
+# Calculate the mean importance for each feature across classifiers after normalization
+average_importances = normalized_importances_df.mean(axis=1)
 
 # Sort the features based on average importance
 sorted_importances = average_importances.sort_values(ascending=False)
@@ -113,236 +116,48 @@ print("Top 5 Most Important Features:")
 print(top_5_features)
 
 
-# RandomForest Feature Importances:
-# redshift       0.203673
-# petroR50_z     0.106085
-# petroR50_u     0.085692
-# petroR50_i     0.084605
-# petroR50_r     0.071088
-# petroR50_g     0.059676
-# psfMag_u       0.047476
-# petroRad_i     0.041347
-# petroRad_u     0.035420
-# petroRad_r     0.032126
-# petroRad_g     0.027775
-# psfMag_r       0.025476
-# psfMag_g       0.017207
-# petroRad_z     0.014627
-# r              0.014359
-# petroFlux_r    0.014145
-# specobjid      0.013948
-# z              0.012549
-# petroFlux_g    0.010687
-# psfMag_i       0.010366
-# psfMag_z       0.009213
-# i              0.007720
-# g              0.007485
-# petroFlux_i    0.007110
-# petroFlux_z    0.006697
-# mjd            0.006095
-# plate          0.005788
-# u              0.005735
-# petroFlux_u    0.005423
-# expAB_z        0.001465
-# expAB_u        0.001239
-# dec            0.001050
-# fiberid        0.001017
-# ra             0.001004
-# expAB_i        0.000974
-# expAB_g        0.000905
-# expAB_r        0.000896
-# field          0.000815
-# run            0.000715
-# camcol         0.000328
-# rerun          0.000000
-# objid          0.000000
+
+# List of top 5 features
+top_features = ['redshift', 'petroR50_g', 'psfMag_u', 'r', 'petroR50_u']
+
+# Dictionary to hold the new values for each feature
+new_values = {}
+
+for feature in top_features:
+    feature_max = data[feature].max()
+    feature_min = data[feature].min()
+    new_value = feature_max + (feature_max - feature_min) * 0.1
+    new_values[feature] = new_value
+
+# Print the new values for the top 5 features
+for feature, value in new_values.items():
+    print(f"New value for {feature} feature: {value}")
+
+
+
+# Calculate the most common value for the top 5 features
+most_common_values = {}
+
+for feature in top_features:
+    valid_data = data[data[feature] != -9999.0]  # Remove missing values
+    most_common_value = valid_data[feature].mode()[0]  # mode() returns a Series, [0] gets the first mode
+    most_common_values[feature] = most_common_value
+
+# Print the most common values for the top 5 features
+for feature, value in most_common_values.items():
+    print(f"Most common value for {feature} feature: {value}")
+
+
+
 # dtype: float64
+# New value for redshift feature: 7.6897864853
+# New value for petroR50_g feature: 1083.4651079999999
+# New value for psfMag_u feature: 27.681467
+# New value for r feature: 33.740267
+# New value for petroR50_u feature: 1195.00843
 
-# XGBoost Feature Importances:
-# petroR50_g     0.764207
-# redshift       0.114395
-# petroR50_u     0.072667
-# psfMag_u       0.006349
-# petroRad_u     0.005928
-# petroR50_i     0.004097
-# petroRad_r     0.003887
-# petroR50_z     0.003224
-# psfMag_z       0.002071
-# petroFlux_g    0.002065
-# expAB_i        0.001818
-# petroRad_z     0.001624
-# petroFlux_z    0.001414
-# psfMag_i       0.001243
-# u              0.001213
-# petroRad_g     0.001163
-# psfMag_g       0.001057
-# petroRad_i     0.000934
-# g              0.000911
-# r              0.000824
-# camcol         0.000702
-# psfMag_r       0.000696
-# expAB_u        0.000681
-# petroFlux_r    0.000590
-# i              0.000574
-# mjd            0.000560
-# z              0.000548
-# expAB_z        0.000543
-# run            0.000535
-# petroFlux_i    0.000522
-# expAB_g        0.000480
-# petroFlux_u    0.000445
-# specobjid      0.000383
-# ra             0.000364
-# field          0.000260
-# fiberid        0.000239
-# plate          0.000233
-# expAB_r        0.000215
-# petroR50_r     0.000209
-# dec            0.000127
-# rerun          0.000000
-# objid          0.000000
-# dtype: float32
-
-# LightGBM Feature Importances:
-# redshift       1270
-# psfMag_u        584
-# expAB_i         353
-# petroFlux_g     308
-# petroR50_u      304
-# expAB_u         301
-# psfMag_g        294
-# u               283
-# fiberid         260
-# expAB_z         255
-# psfMag_z        250
-# expAB_r         248
-# psfMag_i        246
-# expAB_g         242
-# dec             231
-# field           226
-# petroRad_u      218
-# ra              212
-# petroRad_z      192
-# mjd             187
-# petroR50_g      186
-# petroFlux_u     186
-# petroFlux_z     174
-# petroR50_z      170
-# petroRad_g      166
-# run             160
-# psfMag_r        159
-# specobjid       155
-# petroRad_r      146
-# g               144
-# i               136
-# petroR50_i      124
-# z                98
-# petroFlux_r      89
-# r                83
-# petroR50_r       81
-# petroFlux_i      81
-# petroRad_i       75
-# camcol           71
-# plate            52
-# rerun             0
-# objid             0
-# dtype: int32
-
-# CatBoost Feature Importances:
-# redshift       47.699985
-# psfMag_u        5.355378
-# petroRad_g      3.580312
-# petroR50_z      3.417295
-# petroFlux_g     2.571987
-# petroR50_u      2.379830
-# psfMag_z        2.017865
-# petroRad_i      1.868758
-# psfMag_i        1.615840
-# fiberid         1.609646
-# petroRad_z      1.570613
-# g               1.566236
-# petroRad_u      1.465084
-# expAB_z         1.430520
-# u               1.416538
-# psfMag_r        1.372930
-# z               1.322833
-# psfMag_g        1.180268
-# petroFlux_z     1.150006
-# camcol          1.130614
-# dec             1.130467
-# expAB_u         1.125206
-# expAB_i         1.098554
-# i               1.066750
-# field           1.008154
-# run             0.968494
-# petroRad_r      0.928823
-# mjd             0.881245
-# expAB_g         0.872489
-# petroFlux_u     0.850049
-# petroFlux_r     0.744154
-# specobjid       0.686187
-# expAB_r         0.591412
-# petroR50_g      0.556721
-# r               0.533100
-# petroR50_r      0.470955
-# petroFlux_i     0.302106
-# ra              0.262159
-# petroR50_i      0.101776
-# plate           0.098660
-# rerun           0.000000
-# objid           0.000000
-# dtype: float64
-
-# TabNet Feature Importances:
-# r              3.537598e-01
-# petroR50_g     1.861210e-01
-# psfMag_u       1.759128e-01
-# redshift       1.632495e-01
-# petroFlux_i    4.153858e-02
-# petroRad_g     2.330069e-02
-# i              2.328434e-02
-# plate          2.156971e-02
-# dec            3.402002e-03
-# psfMag_g       2.466422e-03
-# objid          2.464259e-03
-# petroR50_i     1.024402e-03
-# specobjid      9.445525e-04
-# petroFlux_u    5.829502e-04
-# petroRad_i     2.886528e-04
-# expAB_z        6.915899e-05
-# petroRad_z     9.782905e-06
-# psfMag_r       8.663509e-06
-# field          1.439237e-06
-# petroR50_z     6.585330e-07
-# expAB_r        2.955775e-07
-# petroR50_u     1.822695e-07
-# petroFlux_r    8.596408e-08
-# petroFlux_g    0.000000e+00
-# ra             0.000000e+00
-# rerun          0.000000e+00
-# expAB_i        0.000000e+00
-# mjd            0.000000e+00
-# expAB_g        0.000000e+00
-# expAB_u        0.000000e+00
-# psfMag_z       0.000000e+00
-# psfMag_i       0.000000e+00
-# fiberid        0.000000e+00
-# z              0.000000e+00
-# u              0.000000e+00
-# petroRad_u     0.000000e+00
-# petroR50_r     0.000000e+00
-# run            0.000000e+00
-# g              0.000000e+00
-# petroRad_r     0.000000e+00
-# petroFlux_z    0.000000e+00
-# camcol         0.000000e+00
-# dtype: float64
-
-# Top 5 Most Important Features:
-# redshift       263.636261
-# psfMag_u       117.917023
-# expAB_i         70.820269
-# petroFlux_g     62.116948
-# petroR50_u      61.307638
-# dtype: float64
-
+# Most common value for redshift feature: 0.0
+# Most common value for petroR50_g feature: 1.948439
+# Most common value for psfMag_u feature: 19.14044
+# Most common value for r feature: 16.63373
+# Most common value for petroR50_u feature: 0.7691579
